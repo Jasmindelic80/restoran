d129b103a78773f1e7efb630770931957aee36ba
Jasmindelic80 <jasmin.delic@gmail.com>
2025-12-27 20:27:04 +0100

    Add root URL response with a greeting

--- restoran/urls.py
+++ restoran/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from django.http import HttpResponse
 from rest_framework.authtoken import views as drf_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', lambda request: HttpResponse("Radi! 🚀")),
     path('korisnici/', include('korisnici.urls')),
     path('meni/', include('meni.urls')),
     path('narudzba/', include('narudzba.urls')),
